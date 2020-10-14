f4b290116b927a3463f3556f135ef0ced953eafc
ousttrue <ousttrue@gmail.com>
2020-10-14 19:22:50 +0900

    tmp.glb

--- run.py
+++ run.py
@@ -1,15 +1,21 @@
-import scene_translator
+import os
 import bpy
+import scene_translator
 
 print('register')
 scene_translator.register()
 
-print('## import ##')
-bpy.ops.scene_translator.importer(filepath='tmp.glb')
-print()
+GLB_FILE = 'tmp.glb'
+
 print('## export ##')
-bpy.ops.scene_translator.exporter(filepath='tmp.glb')
+bpy.ops.scene_translator.exporter(filepath=GLB_FILE)
 print()
+
+print('## import ##')
+bpy.ops.scene_translator.importer(filepath=GLB_FILE)
+print()
+
+os.remove(GLB_FILE)
 
 print('unregister')
 scene_translator.unregister()
