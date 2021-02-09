18e5ea757c15fba9a1c2592a91e40f3c611c0818
ousttrue <ousttrue@gmail.com>
2021-02-09 18:31:14 +0900

    generate vrm-1.0

--- sukonbu_vrm.py
+++ sukonbu_vrm.py
@@ -41,11 +41,43 @@ def main():
             HERE /
             'sukonbu/glTF/extensions/2.0/Khronos/KHR_materials_unlit/schema/gltf.KHR_materials_unlit.schema.json'
         ),
+        # vrm-0
         Generation(
             HERE / 'lib/formats/generated/vrm.py',
             HERE / 'sukonbu/glTF/specification/2.0/schema/glTF.schema.json',
             HERE /
-            'vrm-specification/specification/0.0/schema/vrm.schema.json')
+            'vrm-specification/specification/0.0/schema/vrm.schema.json'),
+        # vrm-1.0
+        Generation(
+            HERE / 'lib/formats/generated/VRMC_vrm.py',
+            HERE / 'sukonbu/glTF/specification/2.0/schema/glTF.schema.json',
+            HERE /
+            'vrm-specification/specification/VRMC_vrm-1.0_draft/schema/VRMC_vrm.schema.json'
+        ),
+        Generation(
+            HERE / 'lib/formats/generated/VRMC_springBone.py',
+            HERE / 'sukonbu/glTF/specification/2.0/schema/glTF.schema.json',
+            HERE /
+            'vrm-specification\specification\VRMC_springBone-1.0_draft\schema\VRMC_springBone.schema.json'
+        ),
+        Generation(
+            HERE / 'lib/formats/generated/VRMC_node_collider.py',
+            HERE / 'sukonbu/glTF/specification/2.0/schema/glTF.schema.json',
+            HERE /
+            'vrm-specification\specification\VRMC_node_collider_1.0_draft\schema\VRMC_node_collider.json'
+        ),
+        Generation(
+            HERE / 'lib/formats/generated/VRMC_materials_mtoon.py',
+            HERE / 'sukonbu/glTF/specification/2.0/schema/glTF.schema.json',
+            HERE /
+            'vrm-specification\specification\VRMC_materials_mtoon-1.0_draft\schema\VRMC_materials_mtoon.schema.json'
+        ),
+        Generation(
+            HERE / 'lib/formats/generated/VRMC_constraints.py',
+            HERE / 'sukonbu/glTF/specification/2.0/schema/glTF.schema.json',
+            HERE /
+            'vrm-specification\specification\VRMC_constraints-1.0_draft\schema\VRMC_constraints.schema.json'
+        ),
     ]
 
     for g in generations:
