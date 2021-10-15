e7aae741c4d5c1460fb3f2ca560e970ee1007454
ousttrue <ousttrue@gmail.com>
2021-10-15 21:34:30 +0900

    unittest

---
+++ tests/test_gltf_load.py
@@ -0,0 +1,41 @@
+import unittest
+import os
+import sys
+from typing import Optional, Iterable
+
+KEY = 'GLTF_SAMPLE_MODELS'
+if KEY not in os.environ:
+    sys.exit()
+import pathlib
+
+SAMPLE_DIR = pathlib.Path(os.environ[KEY]) / '2.0'
+
+
+def get_gltf(d: pathlib.Path) -> Optional[pathlib.Path]:
+    if d.is_dir():
+        for e in d.iterdir():
+            if e.suffix in ('.gltf', '.glb'):
+                return e
+
+
+def iter_gltf(d: pathlib.Path) -> Iterable[pathlib.Path]:
+    for model in d.iterdir():
+        if model.is_dir():
+            for d in model.iterdir():
+                if d.name in ('screenshot', 'README.md'):
+                    continue
+                gltf = get_gltf(d)
+                if gltf:
+                    yield gltf
+
+
+class TestGltf(unittest.TestCase):
+    def test_load(self):
+        for f in iter_gltf(SAMPLE_DIR):
+            print(f.relative_to(SAMPLE_DIR))
+            self.assertEqual(1, 1)
+            break
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_hello.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestHello(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
