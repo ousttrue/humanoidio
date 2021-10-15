2f1e74dd4140ad5cfa0e2da41a28b88c006461a9
ousttrue <ousttrue@gmail.com>
2021-10-15 20:56:34 +0900

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# modelimpex
+
+glTF model importer/exporter
+
---
+++ __init__.py
@@ -0,0 +1,13 @@
+bl_info = {
+    "name": "modelimpex",
+    "blender": (2, 93, 4),
+    "category": "Object",
+}
+
+
+def register():
+    print("Hello World")
+
+
+def unregister():
+    print("Goodbye World")
